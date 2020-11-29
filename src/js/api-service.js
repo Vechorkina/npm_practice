733cc3fc85c5387267ee9ed5052416d41e4a9206
Vechorkina <artdeco.t@i.ua>
2020-11-29 22:56:41 +0200

    Node.JS и NPM
    Node.JS и NPM

---
+++ src/js/api-service.js
@@ -0,0 +1,17 @@
+
+import shortid from 'shortid';
+const fetchAllUsers = () => {
+  console.log('fetchAllUsers');
+};
+const fetchUsersById = () => {
+  console.log('fetchUsersById');
+};
+const updateUsersById = () => {
+  console.log('updateUsersById');
+};
+const x = 5;
+export default { fetchAllUsers, fetchUsersById, updateUsersById, x };
+export const addUser = name => {
+  const user = { id: shortid.generate(), name };
+  console.log(user);
+};
